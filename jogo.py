567abf64405e900faea27544b5500f9a277a31a5
Matheus <theus-7@hotmail.com>
2020-10-17 23:44:23 -0300

    quadrado muda cor se passar a linha

--- jogo.py
+++ jogo.py
@@ -1,14 +1,19 @@
 import pygame as pg
 
 pg.init()
-
+pg.display.set_caption('Muda cor')
 screen = pg.display.set_mode((300, 300))
 
+
 done = False
-is_blue = True
 x = 20
 y = 20
 clock = pg.time.Clock()
+
+font = pg.font.Font('freesansbold.ttf', 16) 
+text = font.render('Passe a linha para mudar de cor', True, (255, 255, 255) , (0, 0, 0) ) 
+textRect = text.get_rect()  
+textRect.center = (150 , 130 ) 
 
 while not done:
     for event in pg.event.get():
@@ -17,8 +22,7 @@ while not done:
 
     pressed = pg.key.get_pressed()
 
-    if pressed[pg.K_a]:
-        is_blue = not is_blue
+
     if pressed[pg.K_UP]:
         y -= 3
     if pressed[pg.K_DOWN]:
@@ -28,14 +32,34 @@ while not done:
     if pressed[pg.K_RIGHT]:
         x += 3
 
-    if is_blue:
-        color = (0, 128, 255)
-    else:
+    if y > 155:
         color = (255, 100, 0)
+    else:
+        color = (0, 128, 255)
+
+    if y < 0:
+        y = 0
+    if y > 270:
+        y = 270
+
+    if x < 0:
+        x = 0
+    if x > 270:
+        x = 270
 
     screen.fill((0, 0, 0))
+    
+    screen.blit(text, textRect) 
+
+    pg.draw.rect(screen, (255, 0, 0), pg.Rect(0, 150, 300, 10))
 
     pg.draw.rect(screen, color, pg.Rect(x, y, 40, 40))
 
     pg.display.flip()
     clock.tick(60)
+
+
+
+
+"""     if pressed[pg.K_a]:
+        is_blue = not is_blue """
